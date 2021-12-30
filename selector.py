41ef64749be4b0cafb0daa44e601c073b2107882
Quhaoh233 <49360609+Quhaoh233@users.noreply.github.com>
2021-12-30 11:30:15 +0800

    Update selector.py

--- selector.py
+++ selector.py
@@ -15,7 +15,7 @@ def type_selector(target_type, type_dict, data_dict):
 
 def RL_selector(target_type, target_poi_density, data_dict, federation_state):
     selector = torch.load('actor_net.pkl')
-    selected_dict = ()
+    selected_dict = dict()
     state = MyFunction.build_state(federation_state, target_type, target_poi_density)
     prob = selector(state)
     action = torch.argmax(prob)
@@ -27,11 +27,24 @@ def RL_selector(target_type, target_poi_density, data_dict, federation_state):
             temp +=1
     return selected_dict
 
-def Eq_selector(data_dict):
-    selected_dict = ()
-    selected_dict[0] = data_dict[0]
-    selected_dict[1] = data_dict[3]
-    selected_dict[2] = data_dict[4]
+def Eq_selector(data_dict, target_num):
+    selected_dict = dict()
+    if target_num == 0:
+        selected_dict[0] = data_dict[0]
+        selected_dict[1] = data_dict[3]
+        selected_dict[2] = data_dict[4]
+    if target_num == 1:
+        selected_dict[0] = data_dict[7]
+        selected_dict[1] = data_dict[9]
+        selected_dict[2] = data_dict[26]
+    if target_num == 2:
+        selected_dict[0] = data_dict[2]
+        selected_dict[1] = data_dict[13]
+        selected_dict[2] = data_dict[24]
+    if target_num == 3:
+        selected_dict[0] = data_dict[12]
+        selected_dict[1] = data_dict[17]
+        selected_dict[2] = data_dict[26]
     return selected_dict
 
 def random_selector(data_dict):
